dafe42080aaccaad8de072b4eb500749ec2159dc
Manuel Schmidt <m.schmidt@complex-it.de>
2020-10-09 07:32:22 +0200

    simplify code

--- background.js
+++ background.js
@@ -1,27 +1,12 @@
 browser.contextMenus.create({
-    id: "compare-image",
-    title: "Compare Image"
+    id: "fetch-iframe",
+    title: "Fetch iFrame"
 });
 
-function messageTab(tabs) {
-    browser.tabs.sendMessage(tabs[0].id, {
-        replacement: "Message from the extension!"
-    });
-}
-
-function onExecuted(result) {
-    var querying = browser.tabs.query({
-        active: true,
-        currentWindow: true
-    });
-    querying.then(messageTab);
-}
-
 browser.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId == "compare-image") {
-        let executing = browser.tabs.executeScript({
-            file: "compare-image.js"
+    if (info.menuItemId == "fetch-iframe") {
+        browser.tabs.executeScript({
+            file: "fetch-iframe.js"
         });
-        executing.then(onExecuted);
     }
 });
---
+++ fetch-iframe.js
@@ -0,0 +1,21 @@
+var iFrameContent = document.querySelector('iframe');
+
+var settings = {
+    mode: 'cors',
+    credentials: 'include',
+    cache: 'default',
+    headers: {
+        'Access-Control-Allow-Origin':'*'
+    }
+};
+
+fetch(iFrameContent.src, settings)
+    .then(response =>  response.text())                      //bringt HTML ins Log und auf die Seite
+    .then(function (data) {
+        var myDiv = document.createElement('div');
+        myDiv.innerHTML = data;
+        console.log(myDiv);
+
+        document.querySelector('.render-wrapper').appendChild(myDiv);
+    })
+
