b07c1d3cd4b07780ef4435dd72234fb2827be135
Manuel Schmidt <m.schmidt@complex-it.de>
2020-10-09 07:38:30 +0200

    simplify code

--- background.js
+++ background.js
@@ -3,8 +3,19 @@ browser.contextMenus.create({
     title: "Fetch iFrame"
 });
 
+browser.contextMenus.create({
+    id: "get-urls",
+    title: "Get Urls"
+});
+
 browser.contextMenus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "fetch-iframe") {
+        browser.tabs.executeScript({
+            file: "fetch-iframe.js"
+        });
+    }
+
+    if (info.menuItemId == "get-urls") {
         browser.tabs.executeScript({
             file: "fetch-iframe.js"
         });
